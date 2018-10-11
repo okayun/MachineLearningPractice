25daa0e1d2585edde606fda7d1235b95f8403889
okayu <y1411199.uec@gmail.com>
2018-10-11 17:00:34 +0900

    Delete Q-Learning and NN

--- Q-Learning-NN/Simple/build.sh
+++
@@ -1,5 +0,0 @@
-g++ main.cpp -std=c++11 -g \
--I $HOME/caffe/include \
--L $HOME/caffe/build/lib \
--lcaffe -lglog -lgflags -lboost_system \
--DCPU_ONLY -lopencv_highgui -lopencv_core -lopencv_imgproc
